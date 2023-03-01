5bf790c4029f653af6868de04e4d9de9f0ad3f98
Ajila K Johnkutty <kjajila@gmail.com>
2023-03-01 09:59:50 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <>
+    <h1>My Favourite foods</h1>
+    <ul>
+      <li>ice cream</li>
+      <li>Chocolate</li>
+      <li>Cake</li>
+    </ul>
+  </>,
+  document.getElementById("root")
+);
