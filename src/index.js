0cae0d7384eb6abd03c314a188d4af34f7ad40e4
Ajila K Johnkutty <kjajila@gmail.com>
2023-03-02 19:30:39 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -28,3 +28,27 @@ ReactDom.render(
   </>,
   document.getElementById("root")
 );
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// const root = document.getElementById("root");
+// const time = new Date(2023,1,2,23).getHours();
+// let msg='';
+// let customColor={
+//   color:''
+// }
+
+// if(time<11){
+//   msg="Good Morning";
+//   customColor.color="red";
+// }
+// else if(time<18){
+//   msg="Good Afternoon";
+//   customColor.color="green";
+// }
+// else{
+//   msg="Good Night";
+//   customColor.color="blue";
+// }
+// ReactDom.render(
+//   <h1 style={customColor}>{msg}</h1>
+// ,root);
